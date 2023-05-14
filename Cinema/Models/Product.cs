af50f54ff425020ad94c12e255152c61e2b07607
Yen Vy <hoyenvy23@gmail.com>
2023-05-14 23:51:17 +0700

    first commit

---
+++ Cinema/Models/Product.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Models
+{
+    public class Product : INotifyPropertyChanged
+    {
+        private string _imageUrl;
+        private string _name;
+        private decimal _price;
+
+        public string ImageUrl
+        {
+            get { return _imageUrl; }
+            set
+            {
+                _imageUrl = value;
+                OnPropertyChanged(nameof(ImageUrl));
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                _price = value;
+                OnPropertyChanged(nameof(Price));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class ViewModel
+    {
+        public ObservableCollection<Product> Products { get; }
+
+        public ViewModel()
+        {
+            // Populate the Products collection with sample data
+            Products = new ObservableCollection<Product>
+            {
+                new Product { ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnR9QkYiPQFCZG2WJ4PPxRBoNFR8fdcitnvg&usqp=CAU", Name = "Product 1", Price = 10.0m },
+                new Product { ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnR9QkYiPQFCZG2WJ4PPxRBoNFR8fdcitnvg&usqp=CAU", Name = "Product 2", Price = 15.0m },
+                new Product { ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnR9QkYiPQFCZG2WJ4PPxRBoNFR8fdcitnvg&usqp=CAU", Name = "Product 3", Price = 20.0m },
+                // Add more products as needed
+            };
+        }
+    }
+}
---
+++ Cinema/Views/Admin/MainAdminWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Cinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Cinema.Views.Admin
+{
+    /// <summary>
+    /// Interaction logic for MainAdminWindow.xaml
+    /// </summary>
+    public partial class MainAdminWindow : Window
+    {
+        DispatcherTimer timer;
+        double panelWidth;
+        bool hidden;
+
+
+        public MainAdminWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 10);
+            timer.Tick += Timer_Tick;
+            panelWidth = sidePanel.Width;
+            ViewModel viewModel = new ViewModel();
+            DataContext = viewModel;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (hidden)
+            {
+                sidePanel.Width -= 1;
+                if(sidePanel.Width <=30)
+                {
+                    timer.Stop();
+                    hidden =true;
+                }
+            }
+        }
+       
+        public void load() {
+
+            
+
+        //    Products = new ObservableCollection<ViewModel>
+        //{
+        //    new ViewModel { ImageUrl = "product1.jpg", Name = "Product 1", Price = 10.0m },
+        //    new ViewModel { ImageUrl = "product2.jpg", Name = "Product 2", Price = 15.0m },
+        //    new ViewModel { ImageUrl = "product3.jpg", Name = "Product 3", Price = 20.0m },
+        //    // Add more products as needed
+        //};
+        }
+    
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+            //menu.IsEnabled = false;
+        }
+
+        //private void hideButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    hideButton.IsEnabled = false;
+        //}
+
+        private void panelHeader_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void ListViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
